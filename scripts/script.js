5e935dcea5fe5432519ee1f973171ed74fbe4f5b
aval512 <andrewvalidis@gmail.com>
2019-05-20 16:24:11 -0500

    js script update

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,61 @@
 $(document).ready(function() {
+
+///////////////////////////////////
+// XMLHttpRequest
+const Url='https://api.{environment}.validis.com/v1/engagements/{engagementId}/connectors/{connectorName}/upload';
+$('.btn').click(function() {
+	$.ajax({
+		url: Url,
+		type: "GET",
+		success: function(result) {
+			console.log(result)
+		},
+		error: function(error) {
+			console.log(`Error ${error}`)
+		}
+	})
+})
+
+// POST
+
+const Url='';
+
+const Data={
+	name: "quickbooks",
+	id: 2
+};
+
+$('.btn').click(function(){
+	$getJSON(Url, function(result) {
+		console.log(result)
+	});
+})
+
+// Fetch
+const Url='';
+
+fetch(Url)
+.then(data=>{return data.json()})
+.then(res=>{console.log(res)})
+
+///////////////////////////////////
+
+	// <a> tag
 	var e = $("div.setup-panel div a"),
+
+		// Form
 		t = $(".setup-content"),
+
+		// Button Variable
 		a = $(".next-btn");
+
 	t.hide(), e.click(function(a) {
 		a.preventDefault();
 		var i = $($(this).attr("href")),
 			s = $(this);
-		s.is("[disabled=disabled]") || (s.parent().prevAll().find("a").addClass("btn-primary"), s.parent().nextAll().find("a").removeClass("btn-primary"), e.removeClass("active").addClass("btn-default"), s.addClass("active"), t.hide(), i.show(), i.find("input:eq(0)").focus())
+		s.is("[disabled=disabled]") || (s.parent().prevAll().find("a").addClass("btn-primary"), 
+		s.parent().nextAll().find("a").removeClass("btn-primary"), e.removeClass("active").addClass("btn-default"), 
+		s.addClass("active"), t.hide(), i.show(), i.find("input:eq(0)").focus())
 	}), a.click(function() {
 		var e = $(this).closest(".setup-content"),
 			t = e.attr("id"),
@@ -17,7 +66,9 @@ $(document).ready(function() {
 		for (var n = 0; n < i.length; n++) i[n].validity.valid || (s = !1, $(i[n]).closest(".form-group").addClass("has-error"));
 		s && a.removeAttr("disabled").trigger("click")
 	}), $("div.setup-panel div a.btn-primary").trigger("click"), $(".form-group i.fa").on("click", function() {
-		$(this).hasClass("fa-eye") ? ($(this).removeClass("fa-eye").addClass("fa-eye-slash"), $(this).parent().find("input").attr("type", "text")) : ($(this).removeClass("fa-eye-slash").addClass("fa-eye"), $(this).parent().find("input").attr("type", "password"))
+		$(this).hasClass("fa-eye") ? ($(this).removeClass("fa-eye").addClass("fa-eye-slash"), 
+		$(this).parent().find("input").attr("type", "text")) : ($(this).removeClass("fa-eye-slash").addClass("fa-eye"), 
+		$(this).parent().find("input").attr("type", "password"))
 	}), $("form input").on("input propertychange paste change", function(e) {
 		$("form .setup-content").each(function() {
 			var e = $(this),
